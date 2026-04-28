4ce32482671c8d2edb4354e2f7c9714f918c24bc
naghamalrjoub <nagham.rjoub@gmail.com>
2026-04-28 22:08:59 +0300

    addition of user and dependent schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -6,6 +6,15 @@ const userSchema = new mongoose.Schema({
     lastName: {type: String, required: true},
     password: {type: String, required: true},
     password: {type: String, required: true},
+    dependent:[{
+        type: mongoose.Schema.Types.ObjectId,
+        unique: true
+    }]
+})
+
+userSchema.pre("save", async function(){
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
 module.exports = mongoose.model("User", userSchema)
