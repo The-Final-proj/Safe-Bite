c8a3fef895071c593bab0206d01ed7cffb0a48f8
naghamalrjoub <nagham.rjoub@gmail.com>
2026-05-01 12:58:53 +0300

    merging

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,14 +3,6 @@ const apiRouter = express.Router()
 
 // routes import
 const userRouter = require("./userRoutes")
-<<<<<<< HEAD
-const reviewRouter = require("./reviewRoutes")
-
-apiRouter.use("/users", userRouter)
-apiRouter.use("/reviews", reviewRouter)
-
-module.exports = apiRouter
-=======
 const productRouter = require("./productRoutes");
 const supplierRouter = require("./supplierRoutes");
 const uploadRouter = require("./uploadRoutes");
@@ -20,4 +12,3 @@ apiRouter.use("/products", productRouter);
 apiRouter.use("/supplier", supplierRouter);
 apiRouter.use("/upload", uploadRouter);
 module.exports = apiRouter;
->>>>>>> 411ac761495d1db01ef412e5257d10a2eba2ce64
