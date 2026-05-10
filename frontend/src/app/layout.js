26ef6b1539918ab6c2f8874373fdf0554211e8a7
rahafabumelish <rahafabumelish@gmail.com>
2026-05-10 12:11:09 +0300

    editing design + success page updates

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
             <CartProvider>
             <AllergensProvider>
               <CategoriesProvider>
+                 <BootstrapClient />  
                 <Navbar />
                     <FavoriteProvider>
                         {children}
--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -1,19 +1,30 @@
-import SignUp from '../../components/SignUp'
-import React from 'react'
-
+import SignUp from "../../components/SignUp";
+import React from "react";
+import Image from "next/image";
 const page = () => {
   return (
     <div className="bg-dark min-vh-100 d-flex align-items-center justify-content-center ">
-        <div className="container bg-light rounded-3 p-0" style={{ maxWidth: "1000px" }}>
-            <div className="row g-0">
-                    <SignUp className= "vw-100"/>
-                    <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center bg-body-secondary">
-                        <span className="text-muted">image here</span>
-                    </div>
+      <div
+        className="container bg-light rounded-3 p-0"
+        style={{ maxWidth: "1000px" }}
+      >
+        <div className="row g-0">
+          <SignUp className="vw-100" />
+          <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center bg-body-secondary">
+            <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center bg-body-secondary">
+              <Image
+                src="/safebite_logo.png"
+                alt="SafeBite Logo"
+                width={350}
+                height={350}
+                style={{ objectFit: "contain" }}
+              />
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
